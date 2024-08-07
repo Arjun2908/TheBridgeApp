59ab672d56716293cd6e18d58b74c365c07810c5
Arjun Gupta <arjungupta2908@gmail.com>
2024-08-07 00:01:23 -0400

    add web app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('settings');
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "assets/.env");
 
   // SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]).then((value) => runApp(const MyApp()));
   runApp(const MyApp());
