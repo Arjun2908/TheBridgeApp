80b596f38ffc8723bada2bfa55be21cd3b5f24dd
Arjun Gupta <arjungupta2908@gmail.com>
2024-10-19 21:45:30 -0400

    add date range filter and enhance notes display in notes page

--- lib/notes/notes_page.dart
+++ lib/notes/notes_page.dart
@@ -8,6 +8,7 @@ import 'package:the_bridge_app/global_helpers.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
+
   @override
   State<NotesPage> createState() => _NotesPageState();
 }
@@ -15,7 +16,7 @@ class NotesPage extends StatefulWidget {
 class _NotesPageState extends State<NotesPage> {
   String _searchQuery = '';
   final TextEditingController _searchController = TextEditingController();
-
+  DateTimeRange? _dateRange;
   @override
   void initState() {
     super.initState();
@@ -28,6 +29,21 @@ class _NotesPageState extends State<NotesPage> {
 
   void _getNotes() {
     context.read<NotesProvider>().fetchNotes();
+  }
+
+  void _selectDateRange() async {
+    DateTimeRange? picked = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+      initialDateRange: _dateRange,
+    );
+
+    if (picked != null) {
+      setState(() {
+        _dateRange = picked;
+      });
+    }
   }
 
   @override
@@ -45,60 +61,114 @@ class _NotesPageState extends State<NotesPage> {
         }
         return Scaffold(
           appBar: AppBar(
-            title: const Text('Saved Notes'),
+            centerTitle: false,
+            title: const Text('Saved Notes', style: TextStyle(fontSize: 22)),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.filter_alt),
+                onPressed: _selectDateRange,
+              ),
+            ],
             bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(50.0),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    hintText: 'Search Notes...',
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+              preferredSize: const Size.fromHeight(70.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                        hintText: 'Search Notes...',
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
           ),
-          body: Consumer<NotesProvider>(
-            builder: (context, notesProvider, child) {
-              List<Note> filteredNotes = notesProvider.notes.where((note) {
-                return note.content.toLowerCase().contains(_searchQuery.toLowerCase());
-              }).toList();
+          body: notesProvider.notes.isEmpty
+              ? const Center(child: Text('No notes available'))
+              : ListView.builder(
+                  itemCount: notesProvider.notes.length,
+                  itemBuilder: (context, index) {
+                    List<Note> filteredNotes = notesProvider.notes.where((note) {
+                      bool matchesSearch = note.content.toLowerCase().contains(_searchQuery.toLowerCase());
+                      bool matchesDate = _dateRange == null || (note.timestamp.isAfter(_dateRange!.start) && note.timestamp.isBefore(_dateRange!.end.add(const Duration(days: 1))));
 
-              return ListView.builder(
-                itemCount: filteredNotes.length,
-                itemBuilder: (context, index) {
-                  final note = filteredNotes[index];
-                  return ListTile(
-                    title: Text(note.content),
-                    subtitle: Text('${steps[note.step].additionalText} - ${formatTimestamp(note.timestamp)}'),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.edit),
-                          onPressed: () {
-                            _showEditNoteDialog(note);
-                          },
+                      return matchesSearch && matchesDate;
+                    }).toList();
+
+                    if (filteredNotes.isEmpty) {
+                      return Container();
+                    }
+
+                    final note = filteredNotes[index];
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      elevation: 3,
+                      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      child: ListTile(
+                        title: Text(
+                          note.content,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            context.read<NotesProvider>().deleteNoteById(note.id!);
-                          },
+                        subtitle: Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text(
+                                'Step: ${steps[note.step].additionalText}',
+                              ),
+                              Text(
+                                formatTimestamp(note.timestamp),
+                                style: TextStyle(color: Colors.grey.shade600),
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                    onTap: () => _showEditNoteDialog(note), // Edit note on tap
-                  );
-                },
-              );
-            },
-          ),
+                        trailing: PopupMenuButton(
+                          onSelected: (value) {
+                            if (value == 'edit') {
+                              _showEditNoteDialog(note);
+                            } else if (value == 'delete') {
+                              context.read<NotesProvider>().deleteNoteById(note.id!);
+                            }
+                          },
+                          itemBuilder: (context) => [
+                            const PopupMenuItem(
+                              value: 'edit',
+                              child: ListTile(
+                                leading: Icon(Icons.edit),
+                                title: Text('Edit'),
+                              ),
+                            ),
+                            const PopupMenuItem(
+                              value: 'delete',
+                              child: ListTile(
+                                leading: Icon(Icons.delete),
+                                title: Text('Delete'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        onTap: () => _showEditNoteDialog(note),
+                      ),
+                    );
+                  },
+                ),
           bottomNavigationBar: BottomNavBar(
-            selectedIndex: 1,
+            selectedIndex: 1, // Adjust index based on your setup
             onItemTapped: (index) => onItemTapped(index, context),
           ),
         );
@@ -144,7 +214,6 @@ class _NotesPageState extends State<NotesPage> {
       focusNode.dispose();
     });
 
-    // Ensure the TextField is focused when the dialog is shown
     Future.delayed(const Duration(milliseconds: 100), () {
       focusNode.requestFocus();
     });
